7b97faccf7a38fc22126b12ea60210e89e58aac8
Vittorio Vissani <vittorio@example.com>
2025-11-12 14:09:04 +0100

    update

--- Exam.R
+++ Exam.R
@@ -7,6 +7,11 @@ library(lubridate)
 files_list <- list.files(pattern = "\\.csv$", full.names = TRUE)
 data <- lapply(files_list, read.csv, stringsAsFactors = FALSE)
 
+#track source file
+for (i in seq_along(data)) {
+  data[[i]]$source <- basename(files_list[i])
+}
+
 conv_str <- function(column_name, df) {
   if (column_name %in% names(df)) {
     df[[column_name]] <- as.character(df[[column_name]])
@@ -34,50 +39,18 @@ to_strings_vec <- setdiff(all_cols, to_nums_vec)
 
 
 data <- lapply(data, function(df)conversions(df, to_strings_vec, to_nums_vec))
+
+
+
 # unisci
 combined <- bind_rows(data)
 
-# riepilogo finale
-cat("\nFinal summary:\n")
-cat("Files:", length(files_list), "\n")
-cat("Rows (merged):", nrow(combined), "\n")
-if (length(conversions) > 0) cat("Columns coerced:", paste(names(conversions), collapse = ", "), "\n")
-
 combined <- combined %>%
   mutate(
-    # rimuove ogni carattere non numerico eccetto 0-9, ., -, e la notazione scientifica e/E e + (es. 9e+07)
-    tender_value_amount = as.numeric(gsub("[^0-9eE+\\.-]", "", as.character(tender_value_amount))),
-  ) %>%
-  filter(!is.na(tender_value_amount) & !is.na(tender_numberOfTenderers))
-combined <- combined %>%
-  mutate(
-    date = as.character(date),
     date = gsub("Z$", "+0000", date),                        # Z -> +0000
     date = gsub("([+-]\\d{2}):(\\d{2})$", "\\1\\2", date),    # +03:00 -> +0300
     date = lubridate::ymd_hms(date, tz = "UTC")               # parse finale
-  ) %>%
-  mutate()
-
-  combined <- bind_rows(data)
-# df: il tuo data.frame
-# cols: vettore di nomi colonna da cercare, es. c("description","title")
-# keywords: vettore di parole chiave, es. c("furniture","arred")
-
-cols <- c("tender_title","tender_description","tender_additionalProcurementCategory")
-keywords <- c("furniture","meuble")
-
-# costruisci pattern (case-insensitive)
-pat <- paste(keywords, collapse = "|")
-
-df_filtered <- combined %>%
-  filter(grepl(pat,
-               paste0(ifelse(is.na(tender_title), "", as.character(tender_title)),
-                      " ",
-                      ifelse(is.na(tender_description), "", as.character(tender_description))),
-               ignore.case = TRUE))
-
-
-
+  ) 
 # legenda
 legend <- c(
   "85_main.csv"  = "Ghana",
@@ -100,18 +73,44 @@ legend <- c(
   "116_main.csv" = "Nigeria",
   "86_main.csv"  = "Nigeria"
 )
-
-options(scipen = 999)
-
-# sostituisci nella colonna 'country'
-df_filtered <- df_filtered %>%
+# sostituisci nella colonna 'source'
+combined <- combined %>%
   mutate(source = recode(source, !!!legend))
+
+sum(is.na(combined$tender_value_currency))
+
+combined <- combined %>%
+  mutate(tender_value_currency = coalesce(
+    tender_value_currency,
+    tender_minValue_currency,               # usa il nome esatto trovato sopra
+    planning_budget_amount_currency
+  ))
+sum(is.na(combined$tender_value_currency))
+cols <- c("tender_minValue_currency", "planning_budget_amount_currency")
+combined <- combined[, !(names(combined) %in% cols)]
+
+  
+#filter for furniture-related tender contenent
+cols <- c("tender_title","tender_description","tender_additionalProcurementCategory")
+keywords <- c("furniture")
+
+# costruisci pattern (case-insensitive)
+pat <- paste(keywords, collapse = "|")
+
+df_filtered <- combined %>%
+  filter(grepl(pat,
+               paste0(ifelse(is.na(tender_title), "", as.character(tender_title)),
+                      " ",
+                      ifelse(is.na(tender_description), "", as.character(tender_description))),
+               ignore.case = TRUE))
+
+
 
 rate_needed <- df_filtered %>%
   select(tender_value_currency, date) %>%
   mutate(
-    date = lubridate::ymd_hms(as.character(date), tz = "UTC"),  # ensure POSIXct
-    date = lubridate::floor_date(date, unit = "month")          # primo giorno del mese
+    date = lubridate::floor_date(date, unit = "month")
+  # primo giorno del mese
   )
 
 # produce la lista minima di month per currency e salva come csv (YYYY-MM-01)
@@ -132,10 +131,10 @@ df_with_rates <- df_filtered %>%
   mutate(month = as.Date(format(date, "%Y-%m-01"))) %>%
   left_join(rates, by = c("tender_value_currency" = "currency", "month" = "month"))
 
-
+options(scipen = 999)
 hist(df_filtered$tender_value_amount)
 
-Distribution <- ggplot(df_filtered, aes(x = source)) +
+Distribution <- ggplot(combined, aes(x = source)) +
   geom_bar(fill = "skyblue") +
   labs(title = "Furniture tender Distribution")
 print(Distribution)
